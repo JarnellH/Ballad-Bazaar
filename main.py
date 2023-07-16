bcc41452b2d18045822b713d31c834d18e6e3d40
Jacob Wood <jake_wood@mac.com>
2023-07-16 08:22:42 -0700

    number separation for prompt storage

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+prompt_storage = []
+
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
@@ -18,14 +20,15 @@ def test_get():
 
 @app.route('/analyze_lyrics', methods=['POST'])
 def analyze_lyrics():
+    global prompt_storage 
     data = request.get_json()
     lyrics = data.get("lyrics")
 
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=f"""
-        Take the given song lyrics and analyze them to derive the main themes and emotions. 
-        Using this analysis, create four distinctive prompts that could be used to inspire scenes in a music video.
+        Take the given song lyrics and analyze them.
+        Using this analysis, generate four distinctive prompts that could be used to inspire scenes in a music video.
         Lyrics: {lyrics}
         """,
         temperature=1,
@@ -35,7 +38,17 @@ def analyze_lyrics():
         presence_penalty=0
     )
 
-    return {"prompts": response.choices[0].text.strip()}
+    prompts = re.split('1\.|2\.|3\.|4\.', response.choices[0].text.strip()) # Separate prompts by numbers
+    prompts = [prompt.strip() for prompt in prompts if prompt]
+
+    for prompt in prompts:
+        prompt_storage.append(prompt)
+
+    return {"prompts": prompts}
+
+@app.route('/get_prompts', methods=['GET'])  # Get stored prompts
+def get_prompts():
+    return {"stored_prompts": prompt_storage}
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3000, debug=True)
